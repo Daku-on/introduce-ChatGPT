6166b675a88a7367da6976e7644ce34f9f993941
Daku-on <113434878+Daku-on@users.noreply.github.com>
2023-03-19 23:55:50 +0900

    とりあえず寝る！

--- app.js
+++ app.js
@@ -4,15 +4,21 @@
 const switcher = document.querySelector('.btn');
 
 // ボタンをクリックしたときの処理
-switcher.addEventListener('click', function() {
+switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
     // ボタンのラベルを切り替える
-const className = document.body.className;
-if (className == "light-theme") { // もし現在ライトテーマの状態だったら
+    const className = document.body.className;
+   // もし現在ライトテーマの状態だったら
+   if (className == "light-theme") {
     this.textContent = "ダークテーマにする";
-} else { // もし現在ダークテーマの状態だったら
+    this.style.color = "white";
+    this.style.backgroundColor = "darkblue";
+
+   } else {  // もし現在ダークテーマの状態だったら
     this.textContent = "ライトテーマにする";
-}
+    this.style.color = "darkblue";
+    this.style.backgroundColor = "white";
+  } 
 });
